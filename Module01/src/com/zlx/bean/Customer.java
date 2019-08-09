979d355872d5516358538f1526f0fdaee74e6b26
zlx94360503 <zlx494360503@hotmail.com>
2019-08-09 22:18:17 +0800

    代码提交

---
+++ Module01/src/com/zlx/bean/Customer.java
@@ -0,0 +1,9 @@
+package com.zlx.bean;
+
+public class Customer {
+
+    public static void main(String[] args) {
+        System.out.println("Customer!!");
+
+    }
+}
---
+++ src/com/zlx/java/Hello.java
@@ -0,0 +1,12 @@
+package com.zlx.java;
+
+/**
+ * @author zlx
+ * @create 2019-08-08 23:16
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+
+    }
+}
